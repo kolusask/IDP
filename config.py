9e6584ac939e44d8ae0652f7c778546fd71edec9
kolusask <19ak97@gmail.com>
2023-05-28 16:43:31 +0200

    Store outputs in a named folder

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+from json import load
+
+
+with open('config.json') as config:
+    CONFIG = load(config)
+
+OUTPUTS_FOLDER = 'outputs/' + CONFIG['CONFIG_NAME']
+
+
+if not os.path.exists(OUTPUTS_FOLDER):
+    os.makedirs(OUTPUTS_FOLDER)
+
+def out_path(p: str):
+    return os.path.join(OUTPUTS_FOLDER, p)
+
+
+if 'src' not in sys.path:
+    sys.path.append('src')
