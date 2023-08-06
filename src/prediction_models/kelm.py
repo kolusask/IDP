d56a9c4985e60ce4ec3396370d6918d6c21b8c3e
Askar Kolushev <19ak97@gmail.com>
2023-08-06 18:51:19 +0200

    Respect batches in KELM

--- src/prediction_models/kelm.py
+++ src/prediction_models/kelm.py
@@ -1,8 +1,5 @@
 import torch
 
-def gaussian_kernel(x1: torch.TensorType, x2: torch.TensorType, gamma: float) \
-        -> torch.TensorType:
-    return torch.exp(-gamma * torch.cdist(x1, x2).pow(2))
 
 class KELM:
     def __init__(self):
@@ -11,16 +8,24 @@ class KELM:
     def fit(self,
             X: torch.TensorType,
             y: torch.TensorType,
-            reg_coeff: float=1.0, gamma: float=1.0, clean: bool=True):
-        omega = gaussian_kernel(X, X, gamma)
-        self._beta = \
-            torch.inverse(torch.eye(X.shape[0]) / reg_coeff + omega) @ y
-        self._data = X
+            reg_coeff: float = 1.0, gamma: float = 1.0, clean: bool = True):
         self._gamma = gamma
-    
+        X_tile = torch.tile(X, (1, X.shape[1])).reshape(*X.shape, -1)
+        omega = torch.exp(-gamma * (X_tile - X_tile.swapaxes(1, 2)).pow(2))
+        self._beta = \
+            torch.bmm(
+                torch.inverse((torch.eye(X.shape[1]) / reg_coeff) + omega),
+                y[..., None]
+                )
+        self._data = X
+
     def __call__(self, X: torch.TensorType) -> torch.TensorType:
         return self.predict(X)
 
     def predict(self, X: torch.TensorType) -> torch.TensorType:
         assert self._beta is not None, "The model is not fitted"
-        return gaussian_kernel(X, self._data, self._gamma) * self._beta
+        return self._kernel(X, self._data) * self._beta
+
+    def _kernel(self, x1: torch.TensorType, x2: torch.TensorType) \
+            -> torch.TensorType:
+        return torch.exp(-self._gamma * torch.norm(x1 - x2).pow(2))
