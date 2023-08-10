f4de13a30690225fb93c72a2bb8c25aa3f7ef08d
Askar Kolushev <19ak97@gmail.com>
2023-08-10 17:22:36 +0200

    Arbitrary dimensions in KELM

--- src/prediction_models/kelm.py
+++ src/prediction_models/kelm.py
@@ -9,14 +9,12 @@ class KELM:
             X: torch.TensorType,
             y: torch.TensorType,
             reg_coeff: float = 1.0, gamma: float = 1.0, clean: bool = True):
+        M, N = X.shape
+        M, C = y.shape
+        assert M == len(X)
         self._gamma = gamma
-        X_tile = torch.tile(X, (1, X.shape[1])).reshape(*X.shape, -1)
-        omega = torch.exp(-gamma * (X_tile - X_tile.swapaxes(1, 2)).pow(2))
-        self._beta = \
-            torch.bmm(
-                torch.inverse((torch.eye(X.shape[1]) / reg_coeff) + omega),
-                y[..., None]
-                )
+        omega = self._kernel(X, X)
+        self._beta = torch.inverse(torch.eye(M) / reg_coeff + omega) @ y
         self._data = X
 
     def __call__(self, X: torch.TensorType) -> torch.TensorType:
@@ -24,8 +22,10 @@ class KELM:
 
     def predict(self, X: torch.TensorType) -> torch.TensorType:
         assert self._beta is not None, "The model is not fitted"
-        return self._kernel(X, self._data) * self._beta
+        return self._kernel(X, self._data) @ self._beta
 
     def _kernel(self, x1: torch.TensorType, x2: torch.TensorType) \
             -> torch.TensorType:
-        return torch.exp(-self._gamma * torch.norm(x1 - x2).pow(2))
+        return torch.exp(
+            -self._gamma * torch.norm(x1[:, None] - x2[None, :], dim=2) ** 2
+        )
