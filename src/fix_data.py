c74650319135ffa7926ce2ca6567d43c4a92a850
Askar Kolushev <19ak97@gmail.com>
2023-09-09 19:08:54 +0200

    Fill first rows when fixing wrong headers

--- src/fix_data.py
+++ src/fix_data.py
@@ -13,22 +13,21 @@ out_dir_path = path.join(argv[2], 'Ampeldaten_20220525_SP')
 sec_subdir_name = 'Detektorzaehlwerte'
 
 
-def prepare_det_paths(det: str):
-    days_dir_path = path.join(det, sec_subdir_name)
-    in_det_path = path.join(in_dir_path, days_dir_path)
-    out_det_path = path.join(out_dir_path, days_dir_path)
-    if not path.exists(out_det_path):
-        makedirs(out_det_path)
+def prepare_sec_paths(sec: str):
+    in_sec_path = path.join(in_dir_path, sec, sec_subdir_name)
+    out_sec_path = path.join(out_dir_path, sec, sec_subdir_name)
+    if not path.exists(out_sec_path):
+        makedirs(out_sec_path)
     
-    return in_det_path, out_det_path
+    return in_sec_path, out_sec_path
 
-def process_headers(in_det_path: str, out_det_path: str):
+def process_headers(in_sec_path: str, out_sec_path: str):
     headers = {}
     files_missing_header = []
     files_with_wrong_header = []
-    for day in listdir(in_det_path):
-        in_day_path = path.join(in_det_path, day)
-        out_day_path = path.join(out_det_path, day)
+    for day in listdir(in_sec_path):
+        in_day_path = path.join(in_sec_path, day)
+        out_day_path = path.join(out_sec_path, day)
         df = read_csv(in_day_path, sep=None, on_bad_lines='skip', engine='python')
         if 'Unnamed' in df.columns[0]:
             df.drop(columns=df.columns[0], axis=1, inplace=True)
@@ -41,12 +40,13 @@ def process_headers(in_det_path: str, out_det_path: str):
             df.set_index('DATUM').to_csv(out_day_path, sep=';')
     assert headers
 
+
     return headers, files_missing_header, files_with_wrong_header
 
-def fix_missing_headers(in_det_path: str, out_det_path: str, files_missing_header: List[str], headers: Dict[int, str]):
+def fix_missing_headers(in_sec_path: str, out_sec_path: str, files_missing_header: List[str], headers: Dict[int, str]):
     for day in files_missing_header:
-        in_day_path = path.join(in_det_path, day)
-        out_day_path = path.join(out_det_path, day)
+        in_day_path = path.join(in_sec_path, day)
+        out_day_path = path.join(out_sec_path, day)
         n_columns = len(read_csv(in_day_path, sep=None, on_bad_lines='skip', engine='python').columns)
         try:
             header = headers[n_columns]
@@ -55,38 +55,46 @@ def fix_missing_headers(in_det_path: str, out_det_path: str, files_missing_heade
         read_csv(in_day_path, sep=None, names=header, on_bad_lines='skip', engine='python')\
             .set_index('DATUM').to_csv(out_day_path, sep=';')
 
-def fix_wrong_headers(in_det_path: str, files_with_wrong_header: List[str], headers: Dict[int, str]):
+def fill_first_rows_with_zeros(in_day_path: str, n_zeros: int):
+    with open(in_day_path, 'r') as file:
+        lines = file.readlines()
+    for i, line in enumerate(lines[1:], start=1):
+        values = line.split(';')
+        if len(values) == 2:
+            lines[i] = values[0] + ';' + ';'.join('0' * n_zeros) + '\n'
+    with open(in_day_path, 'w') as file:
+        file.write(''.join(lines))
+
+def fix_wrong_headers(in_sec_path: str, out_sec_path: str, files_with_wrong_header: List[str], headers: Dict[int, str]):
+    assert len(headers) == 1
+    header = headers[list(headers.keys())[0]]
     for day in files_with_wrong_header:
-        in_day_path = path.join(in_det_path, day)
+        in_day_path = path.join(in_sec_path, day)
+        out_day_path = path.join(out_sec_path, day)
+        fill_first_rows_with_zeros(in_day_path, list(headers.keys())[0] - 1)
         with open(in_day_path, 'r') as csv:
             text = csv.read()
         n_columns = len(text.split('\n')[1].split(';'))
         read_csv(StringIO(text), sep=None, names=headers[n_columns], on_bad_lines='skip', engine='python')\
-            .iloc[1:].set_index('DATUM').to_csv(in_day_path, sep=';')
+            .iloc[1:].set_index('DATUM').to_csv(out_day_path, sep=';')
 
-def process_detector(det: str):
-    in_det_path, out_det_path = prepare_det_paths(det)
-    headers, files_missing_header, files_with_wrong_header = process_headers(in_det_path, out_det_path)
+def process_section(sec: str):
+    in_sec_path, out_sec_path = prepare_sec_paths(sec)
+    headers, files_missing_header, files_with_wrong_header = process_headers(in_sec_path, out_sec_path)
 
-    fix_missing_headers(in_det_path, out_det_path, files_missing_header, headers)
-    fix_wrong_headers(in_det_path, files_with_wrong_header, headers)
+    fix_missing_headers(in_sec_path, out_sec_path, files_missing_header, headers)
+    fix_wrong_headers(in_sec_path, out_sec_path, files_with_wrong_header, headers)
 
-    d20211031 = path.join(out_det_path, 'DetCount_20211031.csv')
+    d20211031 = path.join(out_sec_path, 'DetCount_20211031.csv')
     read_csv(d20211031, sep=';').drop(index=list(range(8, 12))).to_csv(d20211031, sep=';')
 
 
-det_3050_20211017_path = path.join(in_dir_path, '3050', sec_subdir_name, 'DetCount_20211017.csv')
-with open(det_3050_20211017_path, 'r') as det_3050_20211017_file:
-    det_3050_20211017_lines = det_3050_20211017_file.readlines()
-det_3050_20211017_lines[0] = 'DATUM;1(DA1);2(DA2);4(DAL);5(DB1);6(DB2);7(DB3);8(DC1);9(DC2);10(DD1)\n'
-# det_3050_20211017_lines[:5] = [
-#     '2021-10-17 00:00:00;0;0;0;0;0;0;0;0;0',
-#     '2021-10-17 00:15:00;0;0;0;0;0;0;0;0;0',
-#     '2021-10-17 00:30:00;0;0;0;0;0;0;0;0;0',
-#     '2021-10-17 00:45:00;0;0;0;0;0;0;0;0;0',
-# ]
-with open(det_3050_20211017_path, 'w') as det_3050_20211017_file:
-    det_3050_20211017_file.write(''.join(det_3050_20211017_lines))
+sec_3050_20211017_path = path.join(in_dir_path, '3050', sec_subdir_name, 'DetCount_20211017.csv')
+with open(sec_3050_20211017_path, 'r') as sec_3050_20211017_file:
+    sec_3050_20211017_lines = sec_3050_20211017_file.readlines()
+sec_3050_20211017_lines[0] = 'DATUM;1(DA1);2(DA2);4(DAL);5(DB1);6(DB2);7(DB3);8(DC1);9(DC2);10(DD1)\n'
+with open(sec_3050_20211017_path, 'w') as sec_3050_20211017_file:
+    sec_3050_20211017_file.write(''.join(sec_3050_20211017_lines))
 
 def split_section(in_sec: str, split_groups: Dict[str, List[str]], remove_original=True):
     temp_dir_path = path.join(out_dir_path, in_sec + '_unsplit')
@@ -105,28 +113,20 @@ def split_section(in_sec: str, split_groups: Dict[str, List[str]], remove_origin
     if remove_original:
         rmtree(temp_dir_path)
 
-def fill_first_rows_with_zeros(sec: str, day: str, n_zeros: int):
-    file_path = path.join(in_dir_path, sec, sec_subdir_name, f'DetCount_{day}.csv')
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-    for i, line in enumerate(lines[1:], start=1):
-        values = line.split(';')
-        if len(values) == 2:
-            lines[i] = values[0] + ';' + ';'.join('0' * n_zeros) + '\n'
-    with open(file_path, 'w') as file:
-        file.write(''.join(lines))
+# fill_first_rows_with_zeros('3050', '20211017', 9)
+# fill_first_rows_with_zeros('3060', '20210621', 10)
+# fill_first_rows_with_zeros('0036', '20211025', 6)
 
-fill_first_rows_with_zeros('3050', '20211017', 9)
-fill_first_rows_with_zeros('3060', '20210621', 10)
-
-detectors = listdir(in_dir_path)
+sections = listdir(in_dir_path)
 ignore = []
 ignore += ['2040', '4170', '8007']  # empty
-# for i, det in enumerate(detectors):
-for i, det in enumerate(['3050', '3060']):
-    if det not in ignore:
-        print(f'{det} - {i + 1}/{len(detectors)}')
-        process_detector(det)
+ignore += ['6021']                  # conflicting headers
+for i, sec in enumerate(sections, start=1):
+    if sec in ignore:
+        print(f'{sec} - {i}/{len(sections)} - ignored')
+    else:
+        print(f'{sec} - {i}/{len(sections)}')
+        process_section(sec)
 split_section('3060', {
     '3060': ['1(DA1)', '2(DA2)', '3(DB1)'],
     '3060_G': ['4(DD1)', '5(DD2)', '6(DE1)', '9(DF2)', '10(DF1)'],
