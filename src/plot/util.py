f06da4175fe84bdacc2db3ed24a011feba3733a5
kolusask <19ak97@gmail.com>
2023-11-02 15:25:29 +0100

    Limit plots

--- src/plot/util.py
+++ src/plot/util.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 
 
-def plot_trend_and_fft(data, fft_mag, n_plots=5):
+def plot_trend_and_fft(data, fft_mag, n_plots=5, start_int=101):
     data = data.cpu()
     fft_mag = fft_mag.cpu()
     figure, axis = plt.subplots(n_plots, 2)
 
-    for plot, inter in enumerate(range(101, 101 + n_plots)):
+    for plot, inter in enumerate(range(start_int, start_int + n_plots)):
         axis[plot, 0].plot(data[:, inter])
         axis[plot, 0].set_title(f'Int. {inter} - Data')
 
