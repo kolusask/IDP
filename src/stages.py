8329eabd8a3df12532d69777125e8be70190b6ea
kolusask <19ak97@gmail.com>
2024-03-15 21:10:20 +0100

    Full length after compression

--- src/stages.py
+++ src/stages.py
@@ -27,7 +27,7 @@ def extract_data(load_path, start_date: datetime, end_date: datetime, save_path=
 def preprocess_data(threshold, mat_q):
     mat_q_fft = torch.fft.fft(mat_q, dim=0)
     mat_q_fft_mag = torch.abs(mat_q_fft)
-    mat_q_trend_fft = mat_q_fft#.clone()
+    mat_q_trend_fft = mat_q_fft
     mat_q_trend_fft[mat_q_fft_mag < threshold] = 0.0
     mat_q_trend = torch.fft.ifft(mat_q_trend_fft, dim=0)
     mat_q_resid = mat_q - mat_q_trend
@@ -36,8 +36,9 @@ def preprocess_data(threshold, mat_q):
 
 
 def compress_data(mat_q: torch.Tensor, read_period: Period, train_period: Period, alpha: float):
-    mat_q = crop_q_between(mat_q, read_period, train_period)
-    mat_r, nonempty = build_correlation_matrix(mat_q, True)
+    mat_q_cropped = crop_q_between(mat_q, read_period, train_period)
+    mat_r, nonempty = build_correlation_matrix(mat_q_cropped, True)
+    mat_q = mat_q[:, nonempty]
     groups = split_sections_into_groups(mat_r, alpha)
     mat_c, representatives = get_compression_matrix(mat_q, groups)
     mat_x = get_compressed_matrix(mat_c, mat_q)
